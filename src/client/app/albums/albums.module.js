f4b744c2984b840db61931239308b6288be9a640
Shaked Amar <shakedamar@gmail.com>
2016-06-20 08:55:59 +0300

    add gh-pages site

--- src/client/app/albums/albums.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular.module('app.albums', [
-	'app.core'
-  ]);
-})();
--- src/client/app/home/home.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-
-  angular.module('app.home', [
-    'app.core',
-    'app.widgets'
-  ]);
-})();
